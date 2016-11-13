b070e69627fd607101543a2bb3f2bae02780940c
mmorihiro <motakesi54@gmail.com>
2016-11-13 18:00:28 +0900

    #1 Add support for Long, Config and ConfigValue

---
+++ src/main/io.github.config4k/readers/LongReader.kt
@@ -0,0 +1,6 @@
+package io.github.config4k.readers
+
+import com.typesafe.config.Config
+
+
+internal class LongReader : Reader<Long>(Config::getLong)
--- src/test/io/github/config4k/TestExtension.kt
+++ src/test/io/github/config4k/TestExtension.kt
@@ -1,6 +1,9 @@
 package io.github.config4k
 
+import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigValue
+import com.typesafe.config.ConfigValueType
 import io.kotlintest.specs.WordSpec
 
 
@@ -8,23 +11,52 @@ class TestExtension : WordSpec() {
     init {
         "Config.extract" should {
             "return Int value" {
-                val config = ConfigFactory.parseString("""value = 0""")
-                config.extract<Int>("value") shouldBe 0
+                val num = 0
+                val config = ConfigFactory.parseString("""value = $num""")
+                config.extract<Int>("value") shouldBe num
             }
 
             "return String value" {
-                val config = ConfigFactory.parseString("""value = "str" """)
-                config.extract<String>("value") shouldBe "str"
+                val str = "str"
+                val config = ConfigFactory.parseString("""value = $str """)
+                config.extract<String>("value") shouldBe str
             }
 
             "return Boolean value" {
-                val config = ConfigFactory.parseString("""value = true""")
-                config.extract<Boolean>("value") shouldBe true
+                val b = true
+                val config = ConfigFactory.parseString("""value = $b""")
+                config.extract<Boolean>("value") shouldBe b
             }
 
             "return Double value" {
-                val config = ConfigFactory.parseString("""value = 0.1""")
-                config.extract<Double>("value") shouldBe exactly(0.1)
+                val num = 0.1
+                val config = ConfigFactory.parseString("""value = $num""")
+                config.extract<Double>("value") shouldBe exactly(num)
+            }
+
+            "return Long value" {
+                val num = 1000L
+                val config = ConfigFactory.parseString("""value = $num""")
+                config.extract<Long>("value") shouldBe num
+            }
+
+            "return Config" {
+                val inner = """
+                        |{
+                        | field = value
+                        |}""".trimMargin()
+                val config = ConfigFactory.parseString(
+                        """nest = $inner""")
+                config.extract<Config>(
+                        "nest") shouldBe ConfigFactory.parseString(inner)
+            }
+
+            "return ConfigValue" {
+                val b = true
+                val config = ConfigFactory.parseString("value = $b")
+                val configValue = config.extract<ConfigValue>("value")
+                configValue.valueType() shouldBe ConfigValueType.BOOLEAN
+                configValue.unwrapped() shouldBe b
             }
         }
     }
