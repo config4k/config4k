543a7779a02c656e43c5fa9b55b85716ecc8ea82
mmorihiro <motakesi54@gmail.com>
2016-11-20 17:26:08 +0900

    #9 custom exception

---
+++ src/main/io.github.config4k/Config4kException.kt
@@ -0,0 +1,9 @@
+package io.github.config4k
+
+import kotlin.reflect.KClass
+
+
+class Config4kException {
+    class UnSupportedType(type: KClass<*>) :
+            RuntimeException("type: ${type.qualifiedName} is not supported")
+}
