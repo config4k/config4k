84d044cf6f5a37b45ddc25b3bd2881611d2181af
MMorihiro <motakesi54@gmail.com>
2017-02-02 18:19:56 +0900

    #20 Add test case for Map<String, Map<String, T>>

--- src/test/io/github/config4k/TestMap.kt
+++ src/test/io/github/config4k/TestMap.kt
@@ -17,6 +17,27 @@ class TestMap : WordSpec() {
                 val list = mapConfig.extract<Map<String, Int>>("nest")
                 list shouldBe mapOf("key1" to 10, "key2" to 20)
             }
+
+            "return Map<String, Map<String, T>>" {
+                val mapConfig =
+                        ConfigFactory.parseString("""
+                        |nest = {
+                        |  nest1 = {
+                        |   key1 = 10
+                        |   key2 = 20
+                        |}
+                        |  nest2 = {
+                        |   key3 = 30
+                        |   key4 = 40
+                        |}
+                        |}""".trimMargin())
+                val list = mapConfig
+                        .extract<Map<String, Map<String, Int>>>("nest")
+                list shouldBe mapOf(
+                        "nest1" to mapOf("key1" to 10, "key2" to 20),
+                        "nest2" to mapOf("key3" to 30, "key4" to 40)
+                )
+            }
         }
     }
 }
