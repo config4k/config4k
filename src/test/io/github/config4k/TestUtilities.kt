8ca820663e140507082646b2a7f2d8380c127605
hsyed <h.a.syed@gmail.com>
2018-05-17 15:51:17 +0100

    add common binding annotations

--- src/test/io/github/config4k/TestUtilities.kt
+++ src/test/io/github/config4k/TestUtilities.kt
@@ -5,11 +5,9 @@ import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.it
 
 object TestUtilities : Spek({
-    val fixture = """io.github.config4k { test = "boom" } """
-    
     context("Config.forPackageOf<T>()") {
         it("should be able to descend into a config object using the package of a class") {
-            withConfig(fixture) {
+            withConfig("""io.github.config4k { test = "boom" } """) {
                 forPackageOf<TestUtilities>().also {
                     val test: String = it.getString("test")
                     assert(test == "boom")
