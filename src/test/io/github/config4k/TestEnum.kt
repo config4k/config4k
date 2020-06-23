d7b232191be59c33ce4f9cf309819d8d5b70218a
Sergey Morgunov <hostage@me.com>
2020-06-23 15:45:15 +0300

    Revert "Revert "Document delegated properties in README.md""
    This reverts commit 9c57746f

--- src/test/io/github/config4k/TestEnum.kt
+++ src/test/io/github/config4k/TestEnum.kt
@@ -3,7 +3,6 @@ package io.github.config4k
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
-
 class TestEnum : WordSpec({
     "Config.extract<Size>" should {
         "return SMALL" {
