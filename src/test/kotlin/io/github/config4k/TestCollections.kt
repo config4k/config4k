6deca47f6c22302c35062a20de58ca01c8f30d65
Sergey Morgunov <hostage@me.com>
2022-08-24 18:15:50 +0300

    #108 Support `Mutable*` collections

--- src/test/kotlin/io/github/config4k/TestCollections.kt
+++ src/test/kotlin/io/github/config4k/TestCollections.kt
@@ -5,7 +5,6 @@ import io.kotest.matchers.collections.haveSize
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import java.time.Duration
-import java.util.Arrays
 
 class TestCollections : WordSpec({
     val config = "key = [0m, 1m, 1m, 2m]".toConfig()
@@ -17,6 +16,24 @@ class TestCollections : WordSpec({
                 Duration.ofMinutes(1),
                 Duration.ofMinutes(1),
                 Duration.ofMinutes(2)
+            )
+        }
+
+        "return MutableList" {
+            val list = config.extract<MutableList<Duration>>("key")
+            list shouldBe listOf(
+                Duration.ofMinutes(0),
+                Duration.ofMinutes(1),
+                Duration.ofMinutes(1),
+                Duration.ofMinutes(2)
+            )
+            list.add(Duration.ofMinutes(10))
+            list shouldBe listOf(
+                Duration.ofMinutes(0),
+                Duration.ofMinutes(1),
+                Duration.ofMinutes(1),
+                Duration.ofMinutes(2),
+                Duration.ofMinutes(10)
             )
         }
 
@@ -32,25 +49,38 @@ class TestCollections : WordSpec({
             set shouldBe setOf(
                 Duration.ofMinutes(0),
                 Duration.ofMinutes(1),
-                Duration.ofMinutes(1),
                 Duration.ofMinutes(2)
             )
         }
 
+        "return MutableSet" {
+            val setConfig = "key = [1m]".toConfig()
+            val set = setConfig.extract<MutableSet<Duration>>("key")
+            set should haveSize(1)
+            set shouldBe setOf(Duration.ofMinutes(1))
+            set.add(Duration.ofMinutes(10))
+            set should haveSize(2)
+            set shouldBe setOf(
+                Duration.ofMinutes(1),
+                Duration.ofMinutes(10)
+            )
+        }
+
         "return Array<T>" {
-            Arrays.deepEquals(
-                """key = ["a", "b", "c", "d"]""".toConfig()
-                    .extract<Array<String>>("key"),
-                arrayOf("a", "b", "c", "d")
-            ) shouldBe true
-            Arrays.deepEquals(
-                """key = ["0m", "1m"]""".toConfig()
-                    .extract<Array<Duration>>("key"),
-                arrayOf(
-                    Duration.ofMinutes(0),
-                    Duration.ofMinutes(1)
-                )
-            ) shouldBe true
+            """key = ["a", "b", "c", "d"]""".toConfig()
+                .extract<Array<String>>("key")
+                .contentDeepEquals(
+                    arrayOf("a", "b", "c", "d")
+                ) shouldBe true
+
+            """key = ["0m", "1m"]""".toConfig()
+                .extract<Array<Duration>>("key")
+                .contentDeepEquals(
+                    arrayOf(
+                        Duration.ofMinutes(0),
+                        Duration.ofMinutes(1)
+                    )
+                ) shouldBe true
         }
     }
 })
