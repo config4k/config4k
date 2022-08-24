226874916d127b6d578fbfbc7c832818eca19bef
Sergey Morgunov <hostage@me.com>
2022-08-24 13:53:43 +0300

    #101 Support Java Bean notation

---
+++ src/main/kotlin/io/github/config4k/readers/JavaBeanReader.kt
@@ -0,0 +1,8 @@
+package io.github.config4k.readers
+
+import com.typesafe.config.ConfigBeanFactory
+import kotlin.reflect.KClass
+
+internal class JavaBeanReader(clazz: KClass<*>) : Reader<Any?>({ config, path ->
+    ConfigBeanFactory.create(config.getConfig(path), clazz.java)
+})
