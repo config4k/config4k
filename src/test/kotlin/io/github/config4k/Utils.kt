7ade083cbabc7275a9be129acdc0bc0f91307a41
Sergey Morgunov <hostage@me.com>
2025-02-20 17:25:05 +0300

    `com.typesafe.config.ConfigValue` should be serializable

--- src/test/kotlin/io/github/config4k/Utils.kt
+++ src/test/kotlin/io/github/config4k/Utils.kt
@@ -3,8 +3,12 @@ package io.github.config4k
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigRenderOptions
+import com.typesafe.config.ConfigValue
+import com.typesafe.config.ConfigValueFactory
 
 fun String.toConfig(): Config = ConfigFactory.parseString(this.trimIndent())
+
+fun Map<String, Any>.toConfigValue(): ConfigValue = ConfigValueFactory.fromMap(this)
 
 private val RenderOps =
     ConfigRenderOptions
