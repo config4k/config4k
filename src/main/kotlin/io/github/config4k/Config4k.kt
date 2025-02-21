523195a701d9dfd5ceaaf92602d4e2029e693c80
Kornilov Alexandr <targaryen1337@yandex.ru>
2025-02-22 01:32:17 +0300

    kotlin.text.Regex should be serializable

--- src/main/kotlin/io/github/config4k/Config4k.kt
+++ src/main/kotlin/io/github/config4k/Config4k.kt
@@ -3,18 +3,19 @@ package io.github.config4k
 import io.github.config4k.serializers.ConfigSerializer
 import io.github.config4k.serializers.ConfigValueSerializer
 import io.github.config4k.serializers.PeriodSerializer
+import io.github.config4k.serializers.RegexSerializer
 import io.github.config4k.serializers.TemporalAmountSerializer
 import kotlinx.serialization.hocon.Hocon
 import kotlinx.serialization.hocon.serializers.ConfigMemorySizeSerializer
 import kotlinx.serialization.hocon.serializers.JavaDurationSerializer
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
-import kotlinx.serialization.modules.plus
 
 public val Config4kModule: SerializersModule =
     SerializersModule {
         contextual(JavaDurationSerializer)
         contextual(PeriodSerializer)
+        contextual(RegexSerializer)
         contextual(TemporalAmountSerializer)
         contextual(ConfigSerializer)
         contextual(ConfigValueSerializer)
