6afff13938f3e99698b19ca308fea47e7517fee4
Kornilov Alexandr <targaryen1337@yandex.ru>
2025-03-01 01:06:15 +0300

    `java.net.URL` and `java.net.URI` should be serializable

---
+++ src/main/kotlin/io/github/config4k/serializers/URLSerializer.kt
@@ -0,0 +1,21 @@
+package io.github.config4k.serializers
+
+import kotlinx.serialization.KSerializer
+import java.net.URL
+
+/**
+ * Serializer for [URL]
+ *
+ * Usage example:
+ * ```
+ * @Serializable
+ * data class ExampleURL(
+ *   @Serializable(URLSerializer::class)
+ *   val url: URL,
+ * )
+ * val config = ConfigFactory.parseString("url = "https://www.example.com"")
+ * val exampleURL: ExampleURL = Hocon.decodeFromConfig(config)
+ * val newConfig = Hocon.encodeToConfig(ExampleURL.serializer(), exampleURL)
+ * ```
+ */
+public object URLSerializer : KSerializer<URL> by stringSerializer("hocon.${URL::class.qualifiedName}", ::URL, URL::toExternalForm)
